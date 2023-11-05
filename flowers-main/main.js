4a586b6dba35b09ddfa045b4753b980bdd56cc08
Abhishek Singh <110656538+ijabhishek@users.noreply.github.com>
2023-11-05 09:50:51 +0530

    Update main.js

--- flowers-main/main.js
+++ flowers-main/main.js
@@ -1,18 +1,12 @@
-// Remove the class "container" when the page loads
 window.onload = () => {
-        document.body.classList.remove("container");
+  document.body.classList.remove("container");
 
-        document.addEventListener('click', function () {
-                var audio = document.getElementById('background-music');
-                audio.play();
-        });
+  var audio = document.getElementById('background-music');
+
+  document.addEventListener('click', function() {
+    if (audio.paused) {
+      audio.muted = false; // Unmute the audio
+      audio.play(); // Start playback
+    }
+  });
 };
-
-// Pause the background music after 15 seconds
-setTimeout(function () {
-        var backgroundMusic = document.getElementById('background-music');
-        if (backgroundMusic) {
-                backgroundMusic.pause();
-        }
-}, 15000);
-  
