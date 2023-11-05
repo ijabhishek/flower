99c847df990c0aa779aca9329a6f90c5b76c9b63
Abhishek Singh <110656538+ijabhishek@users.noreply.github.com>
2023-11-05 10:08:33 +0530

    flower

---
+++ main.js
@@ -0,0 +1,12 @@
+window.onload = () => {
+        document.body.classList.remove("container");
+
+        var audio = document.getElementById('background-music');
+
+        document.addEventListener('click', function () {
+                if (audio.paused) {
+                        audio.muted = false; // Unmute the audio
+                        audio.play(); // Start playback
+                }
+        });
+};
