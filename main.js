a5ada52600a7e0fcd89473249378392582f7407e
Abhishek Singh <110656538+ijabhishek@users.noreply.github.com>
2023-11-05 10:35:17 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-onload = () => {
-        document.body.classList.remove("container");
+window.onload = () => {
+  document.body.classList.remove("container");
+
+  var audio = new Audio('mysong.mp3');
+
+  document.querySelector('.beautiful-button').addEventListener('click', function() {
+    audio.play();
+    setTimeout(function() {
+      window.location.href = 'flower.html';
+    }, 1000); // Redirect after 1 second (adjust as needed)
+  });
+
+  audio.addEventListener('ended', function() {
+    window.location.href = 'index.html';
+  });
 };
-function playAndRedirect() {
-        var audio = new Audio('mysong.mp3');
-        audio.play();
-        setTimeout(function () {
-                window.location.href = 'flower.html';
-        }, 1000); // Redirect after 1 second (adjust as needed)
-}
