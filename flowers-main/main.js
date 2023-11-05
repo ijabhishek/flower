2971b253085904e39de48874823b90cf93a20901
Abhishek Singh <110656538+ijabhishek@users.noreply.github.com>
2023-11-05 08:58:29 +0530

    flower for her

---
+++ flowers-main/main.js
@@ -0,0 +1,7 @@
+onload = () =>{
+        document.body.classList.remove("container");
+};
+setTimeout(function() {
+        document.getElementById('background-music').pause();
+      }, 15000);
+      
