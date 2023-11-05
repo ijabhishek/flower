976b990b29b8d961d8656bde469aa725df8fc697
Abhishek Singh <110656538+ijabhishek@users.noreply.github.com>
2023-11-05 10:21:50 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
+onload = () => {
+        document.body.classList.remove("container");
+};
 function playAndRedirect() {
-  var audio = new Audio('mysong.mp3');
-  audio.play();
-  window.location.href = 'flower.html';
+        var audio = new Audio('mysong.mp3');
+        audio.play();
+        setTimeout(function () {
+                window.location.href = 'flower.html';
+        }, 1000); // Redirect after 1 second (adjust as needed)
 }
